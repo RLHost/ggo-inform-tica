3cb1fc7378d86b3db6f17e551e3155a87cab1b1d
Rodrigo <irlemos@gmail.com>
2026-05-27 16:30:03 -0300

    AutoDeploy: Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+/** @type {import('tailwindcss').Config} */ export default { content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], theme: { extend: { colors: { brand: { blue: '#0A2540', light: '#E6F0FA', accent: '#0066CC' } } } }, plugins: [] };
